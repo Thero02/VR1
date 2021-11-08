8df2d43dc8be36da8974d8293180cc1e67e9f4d6
Thero02 <69430734+Thero02@users.noreply.github.com>
2021-11-08 13:13:20 +0100

    2.hod

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ import {
   DirectionalLight,
   Vector3,
   Color3,
-  SceneLoader
+  SceneLoader,
+  DeviceOrientationCamera
 } from "@babylonjs/core";
 import "@babylonjs/inspector";
 
@@ -26,32 +27,51 @@ const scene = new Scene(engine);
 // Default Environment
 
 //vytoření kamery v pozici -5 (dozadu)
-const camera = new UniversalCamera("Camera", new Vector3(0, 5, 10), scene);
-
+//const camera = new UniversalCamera("Camera", new Vector3(0, 5, 10), scene);
+//const camera = new UniversalCamera("Camera", new Vector3(1, 1, 1), scene);
+const camera = new DeviceOrientationCamera(
+  "Camera",
+  new Vector3(0, 5, 10),
+  scene
+);
 //zaměřit kameru do středu
-camera.setTarget(Vector3.Zero());
+camera.setTarget(new Vector3(0, 1, 0));
 
 //spojení kamery a grafikcého okna
 camera.attachControl(canvas, true);
 
+// var i = 0;
+// for (i = 0; i < 5; i++) {
+//   // Our built-in 'sphere' shape.
+//   var sphere = MeshBuilder.CreateCylinder(
+//     "sphere",
+//     { diameter: i * 0.2, height: 3, segments: 32 },
+//     scene
+//   );
+//   sphere.position.y = 2;
+//   sphere.position.x = i - 2;
+
+//   if (i === 2) {
+//     var blueMat = new StandardMaterial("blueMat", scene);
+//     blueMat.diffuseColor = new Color3(0.5, 0.5, 0.6);
+//     sphere.material = blueMat;
+//   }
+//}
+
 var i = 0;
 for (i = 0; i < 5; i++) {
-  // Our built-in 'sphere' shape.
   var sphere = MeshBuilder.CreateCylinder(
-    "sphere",
-    { diameter: i * 0.2, height: 3, segments: 32 },
+    "freza",
+    { diameter: 0.2, height: 3 },
     scene
   );
-  sphere.position.y = 2;
-  sphere.position.x = i - 2;
-
+  sphere.position.x = i;
   if (i === 2) {
-    var blueMat = new StandardMaterial("blueMat", scene);
-    blueMat.diffuseColor = new Color3(0.5, 0.5, 0.6);
-    sphere.material = blueMat;
+    var Mat = new StandardMaterial("sedy", scene);
+    Mat.diffuseColor = new Color3(0.5, 0.5, 0.6);
+    sphere.material = Mat;
   }
 }
-
 //světlo
 const light1 = new DirectionalLight(
   "DirectionalLight",
@@ -86,4 +106,4 @@ environment1.setMainColor(new Color3.FromHexString("#74b9ff"));
 environment1.ground.parent.position.y = 0;
 environment1.ground.position.y = 0;
 
-//scene.debugLayer.show();
+scene.debugLayer.show();
